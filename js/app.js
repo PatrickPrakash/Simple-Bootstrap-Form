036b54e7b55f2ad5526c67a0cacdf0bc2dcbd04a
Patrick Prakash <patrickprakash8@gmail.com>
2021-10-01 11:13:01 +0530

    Publishing the Working code on Localhost

--- js/app.js
+++ js/app.js
@@ -308,7 +308,7 @@ bugform.addEventListener('submit', function (e) {
         }
     }
     //Prevent Form from reloading
-    //e.preventDefault();
+    e.preventDefault();
 });
 
 function appScriptPost(formdata) {
